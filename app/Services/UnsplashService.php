3c91196f24f1b52e6038868ab9a894b0b33be68f
Konstantin Savelyev <savelyevks@hotmail.com>
2022-03-26 23:46:00 +0300

    testService

---
+++ app/Services/UnsplashService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use \Unsplash\HttpClient;
+use \Unsplash\Search;
+
+class UnsplashService
+{
+    public $photos;
+
+    public function __construct()
+    {
+        HttpClient::init([
+            'applicationId'    => 'Seqxf62s--OjruqzEE4rByhKkdLDQoW6b3Giw0Juyro',
+            'secret'    => 'B0Ku6OaL1zLZRJ9dpM9kIg6KUrpuH7E7FvNTVKP8JhE',
+            'utmSource' => 'wallfun'
+        ]);
+
+        $search = 'bmw';
+        $page = 3;
+        $per_page = 15;
+        $orientation = 'landscape';
+        $this->photos = Search::photos($search, $page, $per_page, $orientation);
+
+    }
+
+    public function testFunc()
+    {
+        
+    }
+}
